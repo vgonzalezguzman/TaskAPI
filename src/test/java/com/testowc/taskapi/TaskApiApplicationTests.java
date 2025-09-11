30b0624c6457fd536dd3e6b810b4e057bbbe1419
Victor <victor4301esp@gmail.com>
2025-09-11 16:39:39 +0200

    Initial commit

---
+++ src/test/java/com/testowc/taskapi/TaskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testowc.taskapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
